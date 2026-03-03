52bfaab80b49306dc2acb79f13dd9b9e91cf3218
Clayton Burgess <clayton.burgess19@outlook.com>
2026-03-03 17:06:02 -0600

    Initial commit with the struct defined

---
+++ src/block_store.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "bitmap.h"
+#include "block_store.h"
+// include more if you need
+
+struct block_store  {
+        uint8_t *total_stored;  //Blocks off the right amount of storage needed to store the block_store
+        uint8_t *bitmap;        //Tracks each block to determine if it has been allocated or is still free
+        size_t allocated_blocks; //Gives the count of blocks that have been allocated
+};
+
+// You might find this handy. I put it around unused parameters, but you should
+// remove it before you submit. Just allows things to compile initially.
+#define UNUSED(x) (void)(x)
+
+block_store_t *block_store_create()
+{
+	return NULL;
+}
+
+void block_store_destroy(block_store_t *const bs)
+{
+	UNUSED(bs);
+}
+
+size_t block_store_allocate(block_store_t *const bs)
+{
+	UNUSED(bs);
+	return 0;
+}
+
+bool block_store_request(block_store_t *const bs, const size_t block_id)
+{
+	UNUSED(bs);
+	UNUSED(block_id);
+	return false;
+}
+
+void block_store_release(block_store_t *const bs, const size_t block_id)
+{
+	UNUSED(bs);
+	UNUSED(block_id);
+}
+
+size_t block_store_get_used_blocks(const block_store_t *const bs)
+{
+	UNUSED(bs);
+	return 0;
+}
+
+size_t block_store_get_free_blocks(const block_store_t *const bs)
+{
+	UNUSED(bs);
+	return 0;
+}
+
+size_t block_store_get_total_blocks()
+{
+	return 0;
+}
+
+size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
+{
+	UNUSED(bs);
+	UNUSED(block_id);
+	UNUSED(buffer);
+	return 0;
+}
+
+size_t block_store_write(block_store_t *const bs, const size_t block_id, const void *buffer)
+{
+	UNUSED(bs);
+	UNUSED(block_id);
+	UNUSED(buffer);
+	return 0;
+}
+
+block_store_t *block_store_deserialize(const char *const filename)
+{
+	UNUSED(filename);
+	return NULL;
+}
+
+size_t block_store_serialize(const block_store_t *const bs, const char *const filename)
+{
+	UNUSED(bs);
+	UNUSED(filename);
+	return 0;
+}
